0b912ff2ba6af4d843cf144eb51a366e5d98976f
VazhikVM <74149564+VazhikVM@users.noreply.github.com>
2021-10-17 13:37:13 +0300

    2

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,8 +8,60 @@ def index(request):
 
 
 def contact(request):
+
     return render(request, 'mainapp/contact.html')
 
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+
+links_menu = [
+    {'url': 'products',
+     'title': 'Все'
+     },
+    {'url': 'products_home',
+     'title': 'Дом'
+     },
+    {'url': 'products_office',
+     'title': 'Офис'
+     },
+    {'url': 'products_modern',
+     'title': 'Модерн'
+     },
+    {'url': 'products_classic',
+     'title': 'Классика'
+     }
+]
+
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu
+
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu
+
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu
+
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu
+
+    }
+    return render(request, 'mainapp/products.html', context=context)
