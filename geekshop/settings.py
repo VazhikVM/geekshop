276267379f777af9ee5f06877a3c941106896a6b
VazhikVM <74149564+VazhikVM@users.noreply.github.com>
2021-12-04 12:50:16 +0300

    Курс 2 урок 5 часть 2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+r6lg)-dd*0b--t(14)^+=7f#f&-(_de5se3^&)%8fbw0++&o8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -86,10 +86,18 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
 
