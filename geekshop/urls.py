97b88d959d853c6b749a3ee522a43231d296858e
v.vazhinskiy@yandex.ru <Ulenka1990>
2021-12-08 18:15:35 +0300

    урок 6

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from mainapp import views as mainapp
-
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
@@ -32,6 +31,10 @@ urlpatterns = [
 
     path('control/', admin.site.urls),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
