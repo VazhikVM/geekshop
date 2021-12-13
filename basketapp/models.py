3180dabd13c825e4b40eec69a18ebf925353d141
vazhik <v.vazhinskiy@yandex.ru>
2021-12-13 07:51:12 +0300

    часть_1

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product, ProductCategory
+from django.utils.functional import cached_property
 
 
 class Basket(models.Model):
@@ -9,18 +10,22 @@ class Basket(models.Model):
     quantity = models.SmallIntegerField(default=0)
     add_datetime = models.DateTimeField(auto_now_add=True)
 
+    @cached_property
+    def get_item_cached(self):
+        return self.user.basket.select_related()
+
     @property
     def product_cost(self):
         return self.quantity * self.product.price
 
     @property
     def total_quantity(self):
-        _item = Basket.objects.filter(user=self.user)
+        _item = self.get_item_cached
         return sum(list(map(lambda x: x.quantity, _item)))
 
     @property
     def total_cost(self):
-        _item = Basket.objects.filter(user=self.user)
+        _item = self.get_item_cached
         return sum(list(map(lambda x: x.product_cost, _item)))
 
     @staticmethod
