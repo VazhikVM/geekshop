8b7e460451c7d6058d521dd33e26918c99da427b
vazhik <v.vazhinskiy@yandex.ru>
2021-12-16 09:17:40 +0300

    past_2

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.test.client import Client
+from authapp.models import ShopUser
 
-# Create your tests here.
+
+class AuthUserTestCase(TestCase):
+    status_ok = 200
+    status_redirect = 302
+    username = 'django'
+    password = 'geekbrains'
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+        self.superuser = ShopUser.objects.create_superuser(
+            username=self.username,
+            password=self.password,
+        )
+
+    def test_login(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_ok)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.client.login(username=self.username, password=self.password)
+
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, self.status_ok)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+    def test_user_register(self):
+        response = self.client.get('/auth/register/')
+        self.assertEqual(response.status_code, self.status_ok)
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        new_user = {
+            'username': 'Пользователь_2',
+            'first_name': 'Пол_2',
+            'last_name': 'Пол_2',
+            'password1': 'geekbrains',
+            'password2': 'geekbrains',
+            'email': 'polz@geekshop.local',
+            'age': '30'}
+
+        response = self.client.post('/auth/register/', data=new_user)
+        self.assertEqual(response.status_code, self.status_redirect)
+
--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from mainapp.models import ProductCategory, Product
 
-# Create your tests here.
+
+class TestMainappSmoke(TestCase):
+    status_ok = 200
+
+    def setUp(self) -> None:
+        self.category = ProductCategory.objects.create(
+            name='cat_1'
+        )
+
+        for i in range(20):
+            Product.objects.create(
+                name=f'prod-{i}',
+                category=self.category,
+                short_desc=f'shortdesc-{i}',
+                description=f'description-{i}'
+            )
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_ok)
+
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, self.status_ok)
+
+    def test_products_urls(self):
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/product/{product.pk}/')
+            self.assertEqual(response.status_code, self.status_ok)
+
+    def test_categories_urls(self):
+        for category in ProductCategory.objects.all():
+            response = self.client.get(f'/products/category/{category.pk}/')
+            self.assertEqual(response.status_code, self.status_ok)
