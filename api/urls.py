bfe28def42e27a43d17fa053996ad0007579a1ec
vazhik <v.vazhinskiy@yandex.ru>
2021-12-17 09:17:33 +0300

    rest

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ app_name = 'api'
 urlpatterns = [
     path('product/', api.api_product, name='api_product'),
     path('category/', api.api_category, name='api_categories'),
+    path('create/', api.ApiProductCreate.as_view(), name='create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
+import json
+from geekshop import settings
 from mainapp.models import Product, ProductCategory
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from api.serializers import ProductSerializers, CategorySerializers
+from rest_framework.generics import CreateAPIView
 
 
 @api_view(['GET'])
@@ -18,3 +22,23 @@ def api_category(request):
         api_categories = ProductCategory.objects.all()
         serializer = CategorySerializers(api_categories, many=True)
         return Response(serializer.data)
+
+
+class ApiProductCreate(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+    # def create(self, request, *args, **kwargs):
+    #     req_data = request.data.copy()
+    #     with open(f'{settings.BASE_DIR}/geekshop/json/products.json') as f:
+    #         templates = json.load(f)
+    #     print(templates)
+    #     requirements = req_data.pop(templates)
+    #     serializers_data = []
+    #     for requirement in requirements:
+    #         req_data['requirement'] = requirement
+    #         serializer = self.get_serializer(data=req_data)
+    #         serializer.is_valid(raise_exception=True)
+    #         self.perform_create(serializer)
+    #         serializers_data.append(serializer.data)
+    #     return Response(serializers_data, status=status.HTTP_201_CREATED)
